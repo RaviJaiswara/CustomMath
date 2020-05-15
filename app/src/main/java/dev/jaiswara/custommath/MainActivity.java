fa5594655cc8232162de31865f752599e9626a84
Ravi Jaiswara <ravijaiswara92@gmail.com>
2020-05-15 20:42:27 +0530

    Initial commit

---
+++ app/src/main/java/dev/jaiswara/custommath/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.jaiswara.custommath;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ myutils/src/main/java/dev/jaiswara/myutils/MyMath.java
@@ -0,0 +1,23 @@
+package dev.jaiswara.myutils;
+
+public class MyMath {
+    public static int Plus(int a, int b){
+        return a+b;
+    }
+
+    public static int Minus(int a, int b){
+        return a-b;
+    }
+
+    public static int Times(int a, int b){
+        return a*b;
+    }
+
+    public static float Div(int a, int b){
+        if (b == 0){
+            throw new IllegalArgumentException("Div by Zero error");
+        }else {
+            return a/b;
+        }
+    }
+}
